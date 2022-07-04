15046afcf4236759f23611735014630b38250aba
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-07-04 06:57:38 +0545

    July 4 2022

--- Django/MySite03/app1_4/urls.py
+++ Django/MySite03/app1_4/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from app1_4.views import index
+from app1_4.views import *
 
 urlpatterns = [
     path('index/', index),
+    path('blog/', blog),
+    path('carrer/', career),
+    path('contact/', contact),
+    path('offer/', offers),
+    path('onlineadmission/', onlineadmission),
+    path('paymentoption/', paymentoption),
+    path('successgalley/', successgalley),
+    path('testimonials/', testimonials),
 ]
--- Django/MySite03/app1_4/views.py
+++ Django/MySite03/app1_4/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render # html file -> output
+from django.http import HttpResponse # text (html tags) -> output
 # Create your views here.
-
 
 def index(request):
     return render(request, 'app1_4/index.html')
+
+def blog(request):
+    return render(request, 'app1_4/blog.html')
+
+def career(request):
+    return render(request, 'app1_4/career.html')
+
+def contact(request):
+    return render(request, 'app1_4/contact_us.html')
+
+def offers(request):
+    return render(request, 'app1_4/offers.html')
+
+def onlineadmission(request):
+    return render(request, 'app1_4/online_admission.html')
+
+def paymentoption(request):
+    return render(request, 'app1_4/payment_option.html')
+
+def successgalley(request):
+    return render(request, 'app1_4/success_gallery.html')
+
+def testimonials(request):
+    return render(request, 'app1_4/testimonials.html')
+
+# Task-1
+"""
+Create the following html files, render and display.
+    Success Gallery
+    Testimonials
+    Online Admission
+    Payment Option
+    Offers
+    Career
+    Contact Us
+    Blog
+"""

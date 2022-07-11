5a66a25c531d2f43d55656c98a6c29a59349fece
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-07-11 07:20:05 +0545

    July 7 2022 7:20 AM

--- Django/MySite03/app2_1/urls.py
+++ Django/MySite03/app2_1/urls.py
@@ -4,4 +4,6 @@ from app2_1.views import *
 urlpatterns = [
     path('index/', index),
     path('getValues1', getValues1),
+    path('getValues2', getValues2),
+    path('getValues3', getValues3),
 ]
--- Django/MySite03/app2_1/views.py
+++ Django/MySite03/app2_1/views.py
@@ -5,10 +5,43 @@ def index(request):
     return render(request, 'app2_1/index.html')
 
 
+# http://127.0.0.1:8000/app2_1/getValues1?pid=1&name=raj&address=ktm
+# http://127.0.0.1:8000/app2_1/getValues1?pid=1&name=raj&address=LAT
 def getValues1(request):
     # receive process, and output
     pid = request.GET.get('pid') # receive value from url
     name = request.GET.get('name')
-    str1 ="PID : "+str(pid)+"<br/>";
+    address=request.GET['address']
+    data = {'pid':pid,'name':name, 'address':address}
+    # Write on data file (text file)
+    # Insert on database table
+    # Write on xml file
+    # Write on init file
+    # Write on pdf file
+    str1 = "<h1>Personal Info</h1>"
+    str1 =  str1+"ID : "+str(pid)+"<br/>";
     str1 = str1+ "NAME : "+name+"<br/>"
+    str1 = str1 + "ADDRESS : " + address + "<br/>"
+    return HttpResponse(str1)
+
+def getValues2(request):
+    pid = request.GET['txt1']
+    name = request.GET['txt2']
+    address = request.GET['txt3']
+
+    str1 = "<h1>Personal Info</h1>"
+    str1 = str1 + "ID : " + str(pid) + "<br/>";
+    str1 = str1 + "NAME : " + name + "<br/>"
+    str1 = str1 + "ADDRESS : " + address + "<br/>"
+    return HttpResponse(str1)
+
+def getValues3(request):
+    pid = request.POST['txt1']
+    name = request.POST['txt2']
+    address = request.POST.get('txt3')
+
+    str1 = "<h1>Personal Info</h1>"
+    str1 = str1 + "ID : " + str(pid) + "<br/>";
+    str1 = str1 + "NAME : " + name + "<br/>"
+    str1 = str1 + "ADDRESS : " + address + "<br/>"
     return HttpResponse(str1)
